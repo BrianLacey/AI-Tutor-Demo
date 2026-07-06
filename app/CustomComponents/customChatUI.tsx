5f6f455d991cf1b175b2302f647735f705b007c4
Brian Lacey <ss3teddy@gmail.com>
2026-07-06 00:49:14 -0700

    Added basic UI elements for chat with AI.

---
+++ app/CustomComponents/customChatUI.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { BaseSyntheticEvent, useState, useEffect } from "react";
+import { useChat } from "@ai-sdk/react";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationEmptyState,
+} from "@/components/ai-elements/conversation";
+import {
+  Message,
+  MessageContent,
+  MessageResponse,
+} from "@/components/ai-elements/message";
+import {
+  PromptInput,
+  PromptInputTextarea,
+  PromptInputSubmit,
+  type PromptInputMessage
+} from "@/components/ai-elements/prompt-input";
+
+const ChatUI = ({
+  chatLoading = false,
+  setChatLoading = () => {},
+}: {
+  chatLoading: boolean;
+  setChatLoading: any;
+}) => {
+  const [input, setInput] = useState("");
+  const { messages, sendMessage, status } = useChat();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setChatLoading(false);
+  //   }, 4000);
+  // }, []);
+
+  const handlePromptChange = (e: BaseSyntheticEvent) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const handleSubmit = (message:PromptInputMessage, e:BaseSyntheticEvent) => {};
+
+  return (
+    <>
+      <Conversation>
+        <ConversationContent>
+          {messages.length < 1 ? (
+            <ConversationEmptyState className="absolute top-1/2 -translate-y-1/2 -mx-8" />
+          ) : (
+            messages.map((message) => {
+              const { role, parts } = message;
+              return (
+                <Message from={role}>
+                  <MessageContent>
+                    {parts.map((part) =>
+                      part.type === "text" ? (
+                        <MessageResponse>{part.text}</MessageResponse>
+                      ) : null,
+                    )}
+                  </MessageContent>
+                </Message>
+              );
+            })
+          )}
+        </ConversationContent>
+      </Conversation>
+
+      <PromptInput onSubmit={handleSubmit} className="p-4">
+        <PromptInputTextarea
+          value={input}
+          onChange={handlePromptChange}
+          disabled={chatLoading}
+        />
+        <PromptInputSubmit disabled={chatLoading} className="mr-4" />
+      </PromptInput>
+    </>
+  );
+};
+
+export default ChatUI;
